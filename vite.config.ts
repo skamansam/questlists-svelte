67fa35236b77b0d9e027362d5e8540429d1f6fbb
Samuel C. Tyler <skamansam@gmail.com>
2024-11-24 19:19:55 -0500

    add visualizer to build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,19 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite';
 import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 export default defineConfig({
 	plugins: [
-		sveltekit(),
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide'
+		}),
+		sveltekit(),
+		visualizer({
+			filename: 'stats.html',
+			emitFile: true,
+			template: 'flamegraph',
 		})
 	],
 
